49e51930d1c22b495d26a0b1a0d8143f7f9824ad
EvgenyVolenko <evolenko@inbox.ru>
2023-03-15 18:09:42 +0300

    Full HW

--- Seminar_4/Task_03_HW.java
+++ Seminar_4/Task_03_HW.java
@@ -43,8 +43,54 @@
 
 package Seminar_4;
 
+import java.util.ArrayDeque;
+import java.util.Deque;
+import java.util.Scanner;
+
 public class Task_03_HW {
     public static void main(String[] args) {
-        
+        Deque<Double> deque = new ArrayDeque<>();
+        Scanner iScanner = new Scanner(System.in, "Cp866");
+        System.out.print("Введите первое число: ");
+        String otmena = iScanner.next();
+        double nA = Double.parseDouble(otmena);
+        System.out.print("Введите один из знаков операции (+ - / *): ");
+        String zn = iScanner.next();
+        System.out.print("Введите второе число: ");
+        double nB = iScanner.nextDouble();
+        double rez = schet(nA, nB, zn);
+        System.out.printf("Результат вычисления = %s\n", rez);
+        deque.addFirst(rez);
+        System.out.println(deque);
+
+        while (true) {
+            System.out.print("Введите один из знаков операции (+ - / *) или \"отмена\" : ");
+            zn = iScanner.next();
+            if (zn.equals("отмена")) {
+                deque.removeFirst();
+                System.out.println("Предыдущий результат = " + deque.peekFirst());
+            } else {
+                System.out.print("Введите второе число: ");
+                nB = iScanner.nextDouble();
+                rez = schet(deque.peekFirst(), nB, zn);
+                deque.addFirst(rez);
+                System.out.printf("Результат вычисления = %s\n", rez);
+                System.out.println(deque);
+            }
+        }
+    }
+
+    public static double schet(double a, double b, String z) {
+        double r = 0;
+        if (z.equals("+")) {
+            r = a + b;
+        } else if (z.equals("-")) {
+            r = a - b;
+        } else if (z.equals("*")) {
+            r = a * b;
+        } else if (z.equals("/")) {
+            r = a / b;
+        }
+        return r;
     }
 }
