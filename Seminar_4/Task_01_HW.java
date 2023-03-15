d33677dac3a12897f73de8c6fc010e0414dae1e9
EvgenyVolenko <evolenko@inbox.ru>
2023-03-15 16:32:59 +0300

    Half HW

--- Seminar_4/Task_01_HW.java
+++ Seminar_4/Task_01_HW.java
@@ -2,8 +2,36 @@
 
 package Seminar_4;
 
+import java.util.LinkedList;
+
 public class Task_01_HW {
     public static void main(String[] args) {
+        LinkedList<Integer> listD = new LinkedList<>();
+        for (int i = 0; i < 30; i++) {
+            listD.add(i);
+        }
+        System.out.println(listD);
+        System.out.println(revers1(listD));
+        System.out.println(revers2(listD));
+    }
+
+    public static LinkedList<Integer> revers1(LinkedList<Integer> llD) {
+        LinkedList<Integer> llDR = new LinkedList<>();
         
+        for (int i = 0; i < llD.size(); i++) {
+            llDR.addFirst(llD.get(i));
+        }
+
+        return llDR;
+    }
+
+    public static LinkedList<Integer> revers2(LinkedList<Integer> llD) {
+        int temp = 0;
+        for (int i = 0; i < llD.size() / 2; i++) {
+            temp = llD.get(i);
+            llD.set(i, llD.get(llD.size() -1 - i));
+            llD.set((llD.size() -1 - i), temp);
+        }
+        return llD;
     }
 }
--- Seminar_4/Task_02_HW.java
+++ Seminar_4/Task_02_HW.java
@@ -5,8 +5,60 @@
 
 package Seminar_4;
 
+import java.util.LinkedList;
+import java.util.Scanner;
+
 public class Task_02_HW {
     public static void main(String[] args) {
-        
+        LinkedList<Integer> listD = new LinkedList<>();
+        Scanner iScanner = new Scanner(System.in, "Cp866");
+        fillLL(listD);
+        System.out.println(listD);
+        menu();
+        String text = iScanner.next();
+        int nm = Integer.parseInt(text);
+        while (true) {
+            if (nm == 1) {
+                text = iScanner.next();
+                enqueue(text, listD);
+            } else if (nm == 2) {
+                dequeue(listD);
+            } else if (nm == 3) {
+                first(listD);
+            } else {
+                System.out.println("Ввели что-то не то!");
+            }
+            System.out.println(listD);
+            text = iScanner.next();
+            nm = Integer.parseInt(text);
+        }
+    }
+
+    public static void enqueue(String txt, LinkedList<Integer> llD) {
+        int nm = Integer.parseInt(txt);
+        llD.add(nm);
+    }
+
+    public static void dequeue(LinkedList<Integer> llD) {
+        System.out.println("Будет удален перый элемент " + llD.getFirst());
+        llD.removeFirst();
+    }
+
+    public static void first(LinkedList<Integer> llD) {
+        System.out.println("Первый элемент очереди " + llD.getFirst());
+    }
+
+    public static LinkedList<Integer> fillLL(LinkedList<Integer> llD) {
+        for (int i = 0; i < 30; i++) {
+            llD.add(i);
+        }
+        return llD;
+    }
+
+    public static void menu() {
+        System.out.print("Для помещения элемента в конец очереди введите 1\n"
+                + "Для возвращения первого элемента из очереди и удаления его введите 2\n"
+                + "Для возвращения первого элемента из очереди без удаления введите 3\n"
+                + "Введите число: ");
     }
 }
