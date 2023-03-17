753ee29ba24fa42e372206ea87550faa5dfe9eb0
Evgeny <evolenko@inbox.ru>
2023-03-17 22:44:55 +0300

    added full cw and half hw

---
+++ Seminar_5/Task_01_HW.java
@@ -0,0 +1,7 @@
+package Seminar_5;
+
+public class Task_01_HW {
+    public static void main(String[] args) {
+        
+    }
+}
---
+++ Seminar_5/Task_03_HW.java
@@ -0,0 +1,8 @@
+
+package Seminar_5;
+
+public class Task_03_HW {
+    public static void main(String[] args) {
+        
+    }
+}
